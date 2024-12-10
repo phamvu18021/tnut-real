6df393dcbac9f7aa2551ea5271e56f4c278d72c9
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-12-10 15:54:54 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
-const TerserPlugin = require("terser-webpack-plugin");
 
 const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true, // Loại bỏ console.log
-              drop_debugger: true // Loại bỏ debugger
-            },
-            format: {
-              comments: false // Loại bỏ comment
-            }
-          },
-          extractComments: false // Không tạo file riêng cho comment
-        })
-      );
-    }
-    return config;
-  },
-  swcMinify: true,
   reactStrictMode: true,
   swcMinify: true,
   images: {

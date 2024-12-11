a7d4bfe8d588cb06fd41718bfa8e379f1741cbe2
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-12-11 11:42:40 +0700

    update

--- src/features/home/Banner.tsx
+++ src/features/home/Banner.tsx
@@ -31,7 +31,7 @@ export const CardBanner = ({ image }: { image: string }) => {
         layout="responsive"
         width={1950}
         height={800}
-        sizes="(max-width: 768px) 60vw, (max-width: 1200px) 80vw, 100vw"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
         style={{ width: "100%", height: "auto", objectFit: "contain" }}
       />
 
